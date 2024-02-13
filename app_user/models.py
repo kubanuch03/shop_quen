a9a38adea7b5ad0e33c5b0955042736636efb15b
zazaka71 <zazaka71@gmail.com>
2024-02-13 21:25:16 +0600

    first commit

---
+++ app_user/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.core.validators import RegexValidator
+
+from .mangers import UserManager
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(
+        max_length=13,
+        validators=[RegexValidator(r"^\+996\d{9}$")],
+        blank=True,
+        null=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    token_auth = models.CharField(max_length=64, blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self) -> str:
+        return self.email
