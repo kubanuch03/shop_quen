200b6197f6778fd5053a9180f920c2bd7648389e
sadyr7 <sadyr.top@gmail.com>
2024-02-22 23:28:25 +0600

    add discount

--- app_product/urls.py
+++ app_product/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from app_product.views import ListAllProductApiView, CreateProductApiView, ProductDeleteApiView, ProductUpdateApiView, ListOneProducApiView, ProductBySubCategory
+from app_product.views import (ListAllProductApiView, CreateProductApiView, ProductDeleteApiView, 
+ProductUpdateApiView, ListOneProducApiView, ProductBySubCategory, AddDiscountProduct)
 
 urlpatterns = [
     path('list/all/product/', ListAllProductApiView.as_view()),
     path('list/one/product/<int:id>/', ListOneProducApiView.as_view()),
     path('create/product/', CreateProductApiView.as_view()),
-    path('update/product/<int:id>', ProductUpdateApiView.as_view()),
-    path('delete/product/<int:id>', ProductDeleteApiView.as_view()),
+    path('update/product/<int:id>/', ProductUpdateApiView.as_view()),
+    path('delete/product/<int:id>/', ProductDeleteApiView.as_view()),
     path('subcategories/<int:subcategory_id>/products/', ProductBySubCategory.as_view()),
+    path('add/discount/product/<int:id>/', AddDiscountProduct.as_view()),
 ]
--- app_product/views.py
+++ app_product/views.py
@@ -18,7 +18,7 @@ class ListAllProductApiView(ListAPIView):
 class CreateProductApiView(CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductcreateSerializer
-    permission_classes = [IsAuthenticated, ]
+    # permission_classes = [IsAuthenticated, ]
 
 
 
@@ -34,7 +34,25 @@ class ProductUpdateApiView(UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductcreateSerializer
     lookup_field = "id"
-    permission_classes = [IsCreatorOrAdmin, ]
+    # permission_classes = [IsAuthenticated, ]
+
+
+class AddDiscountProduct(UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductcreateSerializer
+    lookup_field = "id"
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        instance = serializer.instance
+        instance.price = serializer.apply_discount_to_price(instance.price, serializer.validated_data.get('discount', 0))
+        instance.save()
 
 
 class ListOneProducApiView(APIView):
