9ed1614b82aa7b56500aef720b0e4dcc198f511c
zazaka71 <zazaka71@gmail.com>
2024-03-21 11:05:37 +0600

    serializers/add product_title add product image

--- app_favorite/serializers.py
+++ app_favorite/serializers.py
@@ -2,10 +2,13 @@ from rest_framework import serializers
 
 from .models import Favorite
 
+from app_product.serializer import ProductListSerializer
 
 class FavoriteSerializer(serializers.ModelSerializer):
-
+    product_image = serializers.ImageField(source='product.images1', read_only=True) 
+    product_title = serializers.CharField(source='product.title', read_only=True) 
     class Meta:
         model = Favorite
-        fields = ['id','user','product','created_at',]
+        fields = ['id','user','product','product_title','product_image','created_at',]
+
 
