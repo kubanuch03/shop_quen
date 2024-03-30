1ebbdf3819642809951626443bb1bca6796b8c30
zazaka71 <zazaka71@gmail.com>
2024-03-30 22:35:44 +0600

    baner

--- app_banner/models.py
+++ app_banner/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Banner(models.Model):
     name = models.CharField(max_length=250)
-    images = models.ImageField(upload_to='banner')
+    images = models.ImageField(upload_to='banner/')
