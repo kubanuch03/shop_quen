7b0175805505f34969e1da2e76d891274bf3f4a4
kubanuch03 <zazaka71@gmail.com>
2024-04-29 15:05:29 +0600

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,4 +18,5 @@ celery -A config beat --loglevel=info --detach
 
 # Ожидаем завершения Gunicorn (или любого другого процесса, запущенного перед)
 wait -n
-service pgbouncer start   
+sudo chmod -R 777 /etc/pgbouncer
+sudo service pgbouncer start   
