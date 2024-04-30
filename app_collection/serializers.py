2473e18fef1e792f5d28607121e173205aaa6efc
kubanuch03 <zazaka71@gmail.com>
2024-04-30 23:07:25 +0600

    fix/image collection

--- app_collection/serializers.py
+++ app_collection/serializers.py
@@ -57,7 +57,7 @@ class NewCollectionListSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         products_queryset = obj.product.all()
         products_data = ProductListSerializer(products_queryset, many=True, context={'request': request}).data
-        for product in products_data:
-            product['images1'] = request.build_absolute_uri(settings.MEDIA_URL + product['images1'])
+        # for product in products_data:
+        #     product['images1'] = request.build_absolute_uri(settings.MEDIA_URL + product['images1'])
         return products_data
     
