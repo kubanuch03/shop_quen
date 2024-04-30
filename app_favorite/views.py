26b6cbcf4cd7d5d8818dc253b89890a521d7fa47
kubanuch03 <zazaka71@gmail.com>
2024-04-30 23:51:30 +0600

    fix/image collection

--- app_favorite/views.py
+++ app_favorite/views.py
@@ -31,7 +31,7 @@ class FavoriteListApiView(generics.ListAPIView):
         return Favorite.objects.none()
     
  
-    @method_decorator(cache_page(7))
+    @method_decorator(cache_page(3))
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
 
