4d7be9b17db0ffb0e984f18f8cbf0b204c67f090
kubanuch03 <zazaka71@gmail.com>
2024-05-01 12:22:32 +0600

    fix/image collection

--- config/settings.py
+++ config/settings.py
@@ -34,20 +34,20 @@ ALLOWED_HOSTS = ['db','*']
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #cors
+    'corsheaders',
     #lib
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'drf_spectacular',
     'django_redis',
-    # 'django_celery_results',
     'drf_yasg',
     'debug_toolbar',
 
@@ -136,6 +136,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+
+
 # import os
 # CACHE_LOCATION = BASE_DIR / 'CACHE' 
 
@@ -184,6 +187,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://queen-shops.com",
     "http://www.queen-shops.com",
     "http://back.queen-shops.com",
+    "http://195.38.164.47",
 
 ]
 
@@ -197,6 +201,7 @@ CORS_ALLOW_ORIGINS = [
     "http://queen-shops.com",
     "http://www.queen-shops.com",
     "http://back.queen-shops.com",
+    "http://195.38.164.47",
 
 
 
