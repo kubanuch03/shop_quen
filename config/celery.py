6d5a261d0856cd34d133c62710eb644a4b8fa127
kubanuch03 <zazaka71@gmail.com>
2024-05-03 22:37:28 +0600

    feat/add auto clear cache celery

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery import shared_task
+from django.core.cache import cache
+from datetime import timedelta
+from celery.schedules import crontab
+
+from pathlib import Path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -8,8 +14,20 @@ app = Celery('config')
 
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
 
+
+
+
+# В файле tasks.py
+
+app.conf.beat_schedule = {
+    'clear_cache_every_10_minutes': {
+        'task': 'app_product.tasks.clear_cache',
+        'schedule': timedelta(minutes=10),
+    },
+}
+
+app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
