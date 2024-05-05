7ae4cda7b6f80dde3bba57823ee05ad31cb57512
kubanuch03 <zazaka71@gmail.com>
2024-05-05 22:11:16 +0600

    cors

--- config/settings.py
+++ config/settings.py
@@ -34,14 +34,13 @@ ALLOWED_HOSTS = ['db','*']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    #cors
-    'corsheaders',
     #lib
     'rest_framework',
     'rest_framework_simplejwt',
