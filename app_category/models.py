236ee77d3094f270ede9044443384804126ac8e0
kubanuch03 <zazaka71@gmail.com>
2024-05-06 03:43:41 +0600

    update

--- app_category/models.py
+++ app_category/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=255)
-    created_at = models.DateField(auto_now_add=True)
-    image = models.ImageField(upload_to="app_category/")
+    title = models.CharField(max_length=255,blank=True,null=True,)
+    created_at = models.DateField(auto_now_add=True,blank=True,null=True,)
+    image = models.ImageField(upload_to="app_category/",blank=True,null=True,)
 
     def __str__(self) -> str:
         return self.title
@@ -18,7 +18,7 @@ class Category(models.Model):
 
 
 class SubCategory(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, blank=True,null=True,on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to="text/")
     creted_at = models.DateField(auto_now_add=True)
