28f2c3cccdaafa90c054b428fceaf94393f65c1e
kubanuch03 <zazaka71@gmail.com>
2024-05-20 22:00:26 +0600

    update

--- app_user/views.py
+++ app_user/views.py
@@ -4,7 +4,6 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 from django.contrib.auth import login, authenticate
 from django.utils import timezone
