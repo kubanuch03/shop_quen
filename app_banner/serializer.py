ec1c923356d859621aa80b85f5013384d9337f9c
kubanuch03 <zazaka71@gmail.com>
2024-05-20 22:13:34 +0600

    fix/serializers baner

--- app_banner/serializer.py
+++ app_banner/serializer.py
@@ -6,6 +6,12 @@ class BannerCRUDserializer(serializers.ModelSerializer):
     class Meta:
         model = Banner
         fields = ["topik_baner"]
+    
+    def to_representation(self, instance):
+        data_product = super().to_representation(instance)        
+        data_product['topik_baner'] = TopikBannerCRUDserializer(instance.topik_baner.all(),many=True).data
+        
+        return data_product  
 
 
 class TopikBannerCRUDserializer(serializers.ModelSerializer):
